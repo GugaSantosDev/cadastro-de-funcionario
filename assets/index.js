7d16a1e0da19ca9160d6772183d2878fed5ffa32
gustavosantos2708 <henriquegustavosantossilva@hotmail.com>
2021-04-06 09:24:04 -0300

    Alterando o form para div e algumas coisas no css da tabela

--- assets/index.js
+++ assets/index.js
@@ -1,39 +1,46 @@
-const botaoSalvar = document.querySelector('.salvar')
+const botaoSalvar = document.querySelector('.salvar');
 const botaoRemover = document.querySelector('.remover');
 const botaoEditar = document.querySelector('.editar');
 const tabela = document.querySelector('.datatable');
 const tbody = document.querySelector('.tbody');
 const inputs = document.querySelectorAll('inputs');
-const alerta = document.querySelector('.alerta')
+const alerta = document.querySelector('.alerta');
 
-function adicionarFuncionario () {
+function adicionarFuncionario(e) {
     const inputs = document.querySelectorAll('input');
 
     let tr = '<tr>';
     let nEmptyInput = 0;
 
-    inputs.forEach(function(input) {
+    inputs.forEach(function (input) {
         if (input.type !== 'button') {
-            if (input.value === '' || input.value === null || input.value === undefined) nEmptyInput++;
+            if (
+                input.value === '' ||
+                input.value === null ||
+                input.value === undefined
+            )
+                nEmptyInput++;
 
             tr += `<td>${input.value}</td>`;
         }
     });
 
     if (nEmptyInput > 0) {
-
-        
-        alerta.style.display= 'block';
-        
-
+        alerta.style.display = 'block';
         return;
-    }else {
+    } else {
         alerta.style.display = 'none';
     }
-    
 
-    tr += `<td><button class='editar'; id='editar'>Editar</button>
-    <button class='remover'; id='remover' onclick='excluirFuncionario(this.rowIndex-1)'>Remover</button></td>`;
+    tr += `<td><button 
+                class='editar'; 
+                id='editar'>Editar
+                </button>
+            <button 
+                class='remover'; 
+                id='remover' 
+                onclick='excluirFuncionario(this.rowIndex-1)'>Remover
+                </button></td>`;
 
     tr += `</tr>`;
 
@@ -42,27 +49,13 @@ function adicionarFuncionario () {
 
     document.querySelector('tbody').appendChild(trElement);
 
-
-    inputs.forEach(function(input) {
+    inputs.forEach(function (input) {
         if (input.type !== 'button') {
-           input.value = '';
+            input.value = '';
         }
     });
-    
-    
-    
-
-};
-
-/*botaoEnviar.addEventListener('click', function() {
-    criaBotoes()
-    
-}); */
+}
 
 function excluirFuncionario(posicaoDaLinha) {
     document.getElementById('tbody').deleteRow(posicaoDaLinha);
-};
-
-
-
-
+}
