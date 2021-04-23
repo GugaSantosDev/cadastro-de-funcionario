d48533a0ed48e9d67a9b65999933b9b0b3ed14a6
gustavosantos2708 <henriquegustavosantossilva@hotmail.com>
2021-04-23 11:32:32 -0300

    Botao remover funcionando perfeitamente

--- assets/index.js
+++ assets/index.js
@@ -5,14 +5,11 @@ const tabela = document.querySelector('.datatable');
 const tbody = document.querySelector('.tbody');
 const inputs = document.querySelectorAll('inputs');
 const alerta = document.querySelector('.alerta');
-
-let idDeFuncionario = 0;
-
+totalFuncionario = 0;
 function adicionarFuncionario(e) {
     const inputs = document.querySelectorAll('input');
     let tr = `<tr>`;
 
-    let contadorTR = 0;
     let nEmptyInput = 0;
 
     inputs.forEach(function (input) {
@@ -33,27 +30,31 @@ function adicionarFuncionario(e) {
         return;
     } else {
         alerta.style.display = 'none';
-        console.log(idDeFuncionario++);
-        contadorTR++;
+        //totalFuncionario++;
+        // console.log({ totalFuncionario });
     }
 
-    tr += `<td><button 
-                class='editar'; 
-                id='editar'>
-                Editar
+    tr += `<td>
+                <button 
+                    class='editar'
+                    id='editar'
+                >
+                    Editar
                 </button>
-            <button 
-                class='remover'; 
-                id='remover' 
-                onclick='excluirFuncionario(-1, ${idDeFuncionario})'>
-                Remover
-                </button></td>`;
+                <button 
+                    class='remover'; 
+                    id='remover' 
+                    onclick='excluirFuncionario(this)'
+                >
+                    Remover
+                </button>
+            </td>`;
 
     tr += `</tr>`;
 
     const trElement = document.createElement('tr');
     trElement.innerHTML = tr;
-    trElement.setAttribute('id', `${idDeFuncionario}`);
+    trElement.setAttribute('id', `${totalFuncionario}`);
 
     document.querySelector('tbody').appendChild(trElement);
 
@@ -64,6 +65,7 @@ function adicionarFuncionario(e) {
     });
 }
 
-function excluirFuncionario(index) {
-    document.getElementById('tbody').deleteRow(index);
+function excluirFuncionario(button) {
+    const tr = button.parentNode.parentNode;
+    document.getElementById('tbody').deleteRow(tr.rowIndex - 1);
 }
